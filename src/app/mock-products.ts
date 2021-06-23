574a12d605067fd167fdc1c51de2a3e1333bc7ed
NIMROD GABBAY <nimrod240597@gmail.com>
2021-06-23 13:47:12 +0300

    uploading files

---
+++ src/app/mock-products.ts
@@ -0,0 +1,13 @@
+import { Product } from "./product";
+export const PRODUCTS: Product[] = [
+    { id: 1, name: 'Banana' ,price: 5.9},
+    { id: 2, name: 'Watermelon',price: 34.8 },
+    { id: 3, name: 'Tomato',  price: 7.1 },
+    { id: 4, name: 'Cucumber', price:3.4 },
+    { id: 5, name: 'Mango' , price: 5.5},
+    { id: 6, name: 'Olive', price: 1.6 },
+    { id: 7, name: 'Avocado',price:5 },
+    { id: 8, name: 'Onion', price: 2.9 },
+    { id: 9, name: 'Pineapple' ,price:60},
+    { id: 10, name: 'Apple',price: 6 }
+  ];
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import { PRODUCTS } from '../mock-products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products=PRODUCTS;
+  selectedProduct?: Product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSelect(product:Product): void {
+    this.selectedProduct = product; 
+  }
+
+}
